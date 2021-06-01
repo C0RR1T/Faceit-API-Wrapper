b28bdf8fdaf2987ffb0e7bcc4e38d5d229d28d1d
CORR1T <corsin@ragett.li>
2021-06-01 16:20:35 +0200

    Started to work

---
+++ src/models/match/StatsOfMatch.ts
@@ -0,0 +1,10 @@
+export default interface StatsOfMatch {
+    map: string,
+    score: string,
+    result: string,
+    elo: string,
+    kills: number,
+    deaths: number,
+    killDeath: number,
+    eloDiff: number
+}
---
+++ src/models/player/PlayerStats.ts
@@ -0,0 +1,13 @@
+export default interface PlayerStats {
+    winRate: number,
+    recentResults: string[],
+    headShotPercentage: number,
+    currentWinStreak: number,
+    wins: number,
+    killDeath: number,
+    averageHSPercentage: number,
+    longesWinStreak: number,
+    matches: number,
+    averageKillDeath: number
+}
+
